5be2c858a8806baca86cefb41c6594aa361bc32e
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-08-17 14:52:50 -0700

    add browser router and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import App from './App'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 import store from './store'
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
 
