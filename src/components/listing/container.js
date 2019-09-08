a959db88f38634c922dc431e8d77c1952f292971
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-07 18:37:47 -0700

    update with how many results

--- src/components/listing/container.js
+++ src/components/listing/container.js
@@ -24,9 +24,16 @@ const listings = [
 ]
 
 class ListingContainer extends React.Component {
+  state = {
+    batchSize: 20
+  }
+
   render() {
     return(
       <div className='listingContainer'>
+        <div className='listingContainerResultsCounter'>
+          Menunjukkan {this.state.batchSize < listings.length ? this.state.batchSize : listings.length} dari {listings.length} listings
+        </div>
         {
           listings.map(listing => {
             return (
