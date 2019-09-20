39c646c10e8947bc2e635e5c055c35f6686c7785
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-19 23:17:00 -0700

    move things over to pages directory

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import './App.css'
 import Header from './components/header/header'
-import HomeContainer from './components/home-container'
-import SearchContainer from './components/search-container'
+import HomePage from './pages/home-container'
+import SearchPage from './components/search-container'
 
 class App extends React.Component {
   render() {
@@ -13,8 +13,8 @@ class App extends React.Component {
         <Header />
         <div className='AppBody'>
           <BrowserRouter>
-            <Route exact path='/' component={HomeContainer} />
-            <Route path='/search' component={SearchContainer} />
+            <Route exact path='/' component={HomePage} />
+            <Route path='/search' component={SearchPage} />
           </BrowserRouter>
         </div>
       </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import './home-container.css'
-import BackgroundImage from './backgroundImage'
-import Autocomplete from './autocomplete/autocomplete'
+import './home.css'
+import BackgroundImage from './../components/backgroundImage'
+import Autocomplete from './../components/autocomplete/autocomplete'
 
 class HomeContainer extends React.Component {
   render() {
     return (
-      <div className='homeContainer'>
+        <div className='homeContainer'>
         <BackgroundImage backgroundImage={'http://c1.peakpx.com/wallpaper/551/3/749/architecture-family-house-front-yard-wallpaper-preview.jpg'} />
         <h1>Cari rumah dibuat gampang</h1>
         <Autocomplete className='autocompleteContainer' />
-      </div>
+        </div>
     )
   }
 }
