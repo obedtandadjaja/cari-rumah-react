3ab71938e066e46f83d8cb7cd5ca1eb49f825882
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-09-21 00:26:03 -0700

    make autocomplete send authorization header

--- src/api/autocomplete.js
+++ src/api/autocomplete.js
@@ -6,10 +6,18 @@ import {
 } from './../actions/autocompleteActions';
 
 export function getAutocompletePredictions(input) {
+  let jwt = window.localStorage.getItem('jwt') || ''
+
   return dispatch => {
     dispatch(getAutocompletePredictionsBegin())
-    axios.get(`http://localhost:4000/google/autocomplete?input=${input}`)
-      .then(res => {
+    axios.get('http://localhost:4000/google/autocomplete', {
+      params: {
+        input: input
+      },
+      headers: {
+        authorization: `Bearer ${jwt}`
+      }
+    }).then(res => {
         dispatch(getAutocompletePredictionsSuccess(res.data))
         return res.data
       })
