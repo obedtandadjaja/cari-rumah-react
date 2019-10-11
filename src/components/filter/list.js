566e4adddacda62cac463de95e58c8b10ba3fe38
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2019-10-11 00:22:48 -0700

    when small enough screen put autocomplete back to filter list

--- src/components/filter/list.js
+++ src/components/filter/list.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
 import './list.css'
-import FilterPriceContainer from './../filter/priceContainer'
+import Autocomplete from './../autocomplete/autocomplete'
 import FilterBedroomsContainer from './../filter/bedroomsContainer'
 import FilterBathroomsContainer from './../filter/bathroomsContainer'
-import FilterTypesContainer from './../filter/typesContainer'
+import FilterPriceContainer from './../filter/priceContainer'
 import FilterResidentialTypesContainer from './../filter/residentialTypesContainer'
+import FilterTypesContainer from './../filter/typesContainer'
 
 function FilterList(props) {
   return (
     <div className='filterList'>
+      {
+        window.innerWidth <= 900 &&
+          <Autocomplete mini={true} />
+      }
       <FilterPriceContainer />
       <FilterBedroomsContainer />
       <FilterBathroomsContainer />
